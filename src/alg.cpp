8ef25c64f7186818fa8021ec7a74eeee80ecfba2
jidkiy-alt <76746846+jidkiy-alt@users.noreply.github.com>
2021-05-28 18:22:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,33 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> tree;
+    std::string word;
+
+    std::ifstream file(filename);
+
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return;
+    }
+
+    while (!file.eof())
+    {
+        int ch = file.get();
+
+        if (ch >= 'A' && ch <= 'z') {
+            if (ch >= 'a' && ch <= 'z') {
+                ch + 26;
+            }
+            word += ch;
+        }
+        else if (!word.empty()) {
+            tree.add(word);
+            word = "";
+        }
+    }
+
+    file.close();
+
+    return tree;
 }
